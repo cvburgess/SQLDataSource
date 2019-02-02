5c8a93614a2a1f5c441ac8fa0cf5d71fc8cdde17
tab00 <tab00@users.noreply.github.com>
2019-02-02 20:50:38 +0630

    Added check for database client in getBatched() in case raw query result transformation to array is needed
    The result of a Knex raw query on a PostgresSQL database does not return an array, so transformation is required.
    Other cases may need to be added for other databases.
    Transformation is not required if the database is SQLite.
    
    Refactored code e.g.:
    Added "isBatched" parameter to getCached(), so that code is reused and getBatchedAndCached() is no longer needed.
    getBatched() and getCached() now return promises to allow chaining.

--- SQLCache.js
+++ SQLCache.js
@@ -4,42 +4,47 @@ const DataLoader = require("dataloader");
 class SQLCache {
   constructor(cache = new InMemoryLRUCache(), knex) {
     this.cache = cache;
+    this.knex = knex;
     this.loader = new DataLoader(rawQueries =>
       Promise.all(rawQueries.map(rawQuery => knex.raw(rawQuery)))
     );
   }
 
-  getBatched(query) {
+  async getBatched(query) {
     const queryString = query.toString();
-    return this.loader.load(queryString).then(result => result && result.rows);
+
+    console.log(`In getBatched. queryString: ${JSON.stringify(queryString)}`)
+
+    return this.loader.load(queryString)
+          .then((loaderResult) => {
+        switch (this.knex.client) {
+          case "pg":
+            return loaderResult && loaderResult.rows;
+          default:
+            return loaderResult;
+        }
+      })
   }
 
-  getCached(query, ttl) {
+  async getCached(query, ttl, isBatched) {
     const queryString = query.toString();
     const cacheKey = `sqlcache:${queryString}`;
 
-    return this.cache.get(cacheKey).then(entry => {
-      if (entry) return Promise.resolve(entry);
-      return query.then(rows => {
-        if (rows) this.cache.set(cacheKey, rows, ttl);
-        return Promise.resolve(rows);
-      });
-    });
-  }
+    console.log(`In getCached. queryString: ${JSON.stringify(queryString)}`)
 
-  getBatchedAndCached(query, ttl) {
-    const queryString = query.toString();
-    const cacheKey = `sqlcache:${queryString}`;
+    return this.cache.get(cacheKey)
+      .then(entry => {
+        if (entry) {
+          console.log(`cache entry: ${JSON.stringify(entry)}`);
+          return entry;
+        }
 
-    return this.cache.get(cacheKey).then(entry => {
-      if (entry) return Promise.resolve(entry);
-      return this.loader
-        .load(queryString)
-        .then(result => result && result.rows)
-        .then(rows => {
-          if (rows) this.cache.set(cacheKey, rows, ttl);
-          return Promise.resolve(rows);
-        });
+        return (isBatched ? this.getBatched(query) : query)
+          .then((queryResult) => {
+            if (queryResult) this.cache.set(cacheKey, queryResult, ttl);
+
+            return queryResult;
+          });
     });
   }
 }
