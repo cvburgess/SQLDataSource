e3f0bbc83fc18f8ac12205ae74da2235f8938114
tab00 <tab00@users.noreply.github.com>
2019-02-07 16:21:20 +0630

    Added check for database client in getBatched()
    In case raw query result transformation to array is needed.

--- SQLCache.js
+++ SQLCache.js
@@ -4,6 +4,7 @@ const DataLoader = require("dataloader");
 class SQLCache {
   constructor(cache = new InMemoryLRUCache(), knex) {
     this.cache = cache;
+    this.knex = knex;
     this.loader = new DataLoader(rawQueries =>
       Promise.all(rawQueries.map(rawQuery => knex.raw(rawQuery)))
     );
@@ -11,8 +12,17 @@ class SQLCache {
 
   getBatched(query) {
     const queryString = query.toString();
-    return this.loader.load(queryString).then(result => result && result.rows);
-  }
+
+    return this.loader.load(queryString)
+      .then((loaderResult) => {
+        switch (this.knex.client) {
+          case "pg":
+            return loaderResult && loaderResult.rows;
+          default:
+            return loaderResult;
+        }
+      })
+}
 
   getCached(query, ttl) {
     const queryString = query.toString();
@@ -33,12 +43,12 @@ class SQLCache {
 
     return this.cache.get(cacheKey).then(entry => {
       if (entry) return Promise.resolve(entry);
-      return this.loader
-        .load(queryString)
-        .then(result => result && result.rows)
-        .then(rows => {
-          if (rows) this.cache.set(cacheKey, rows, ttl);
-          return Promise.resolve(rows);
+
+      return this.getBatched(query)
+        .then((queryResult) => {
+          if (queryResult) this.cache.set(cacheKey, queryResult, ttl);
+
+          return Promise.resolve(queryResult);
         });
     });
   }
