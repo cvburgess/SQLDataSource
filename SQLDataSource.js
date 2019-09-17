a8e16ea176779c2755695a0d3ae5b0e5e653d2ae
Charles Burgess <consulting@cvburgess.com>
2019-09-17 09:36:50 -0400

    remove files and update changelog

--- SQLDataSource.js
+++ SQLDataSource.js
@@ -1,8 +1,7 @@
 const { DataSource } = require("apollo-datasource");
+const { InMemoryLRUCache } = require("apollo-server-caching");
 const Knex = require("knex");
 const knexTinyLogger = require("knex-tiny-logger").default;
-
-const SQLCache = require("./SQLCache");
 
 const { DEBUG } = process.env;
 
@@ -12,22 +11,29 @@ class SQLDataSource extends DataSource {
   constructor(knexConfig) {
     super();
 
-    this.sqlCache = new SQLCache();
+    this.cache = new InMemoryLRUCache();
     this.db = Knex(knexConfig);
 
-    const sqlCache = this.sqlCache;
     Knex.QueryBuilder.extend("cache", function(ttl = 60) {
-      return sqlCache.getCached(this, ttl);
+      const cacheKey = this.toString();
+
+      if (DEBUG && !hasLogger) {
+        hasLogger = true; // Prevent duplicate loggers
+        knexTinyLogger(this.db); // Add a logging utility for debugging
+      }
+
+      return this.cache.get(cacheKey).then(entry => {
+        if (entry) return Promise.resolve(entry);
+        return this.then(rows => {
+          if (rows) this.cache.set(cacheKey, rows, { ttl });
+          return Promise.resolve(rows);
+        });
+      });
     });
   }
 
   initialize(config) {
-    this.sqlCache = new SQLCache(config.cache, this.db);
-
-    if (DEBUG && !hasLogger) {
-      hasLogger = true; // Prevent duplicate loggers
-      knexTinyLogger(this.db); // Add a logging utility for debugging
-    }
+    if (config.cache) this.cache = config.cache;
   }
 }
 
