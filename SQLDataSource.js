7316f7a95af1e0b676b23f206b25a09dd2aad56f
Charles Burgess <consulting@cvburgess.com>
2019-09-16 23:13:44 -0400

    migrate to knex extensions

--- SQLDataSource.js
+++ SQLDataSource.js
@@ -1,34 +1,33 @@
 const { DataSource } = require("apollo-datasource");
+const Knex = require("knex");
 const knexTinyLogger = require("knex-tiny-logger").default;
+
 const SQLCache = require("./SQLCache");
 
 const { DEBUG } = process.env;
 
 let hasLogger = false;
 
-const configError = Error("You must set this.db to a valid knex instance");
-
 class SQLDataSource extends DataSource {
-  initialize(config) {
-    this.context = config.context;
-    this.db = this.db || this.knex;
+  constructor(knexConfig) {
+    super();
 
-    if (!this.db) throw configError;
-    if (this.knex) {
-      // eslint-disable-next-line
-      console.warn("this.knex has been deprecated, use this.db instead");
-    }
+    this.sqlCache = new SQLCache();
+    this.db = Knex(knexConfig);
+
+    const sqlCache = this.sqlCache;
+    Knex.QueryBuilder.extend("cache", function(ttl = 60) {
+      return sqlCache.getCached(this, ttl);
+    });
+  }
+
+  initialize(config) {
+    this.sqlCache = new SQLCache(config.cache, this.db);
 
     if (DEBUG && !hasLogger) {
       hasLogger = true; // Prevent duplicate loggers
       knexTinyLogger(this.db); // Add a logging utility for debugging
     }
-
-    this.sqlCache = new SQLCache(config.cache, this.db);
-    this.getBatched = query => this.sqlCache.getBatched(query);
-    this.getCached = (query, ttl) => this.sqlCache.getCached(query, ttl);
-    this.getBatchedAndCached = (query, ttl) =>
-      this.sqlCache.getBatchedAndCached(query, ttl);
   }
 }
 
